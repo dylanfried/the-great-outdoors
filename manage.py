565cdf7fe2fa2d07deafc9fc410c9d6aee253c92
Dylan Fried <dylanfried@Dylans-MacBook-Pro.local>
2016-04-16 12:47:02 -0400

    got rid of server

--- manage.py
+++ manage.py
@@ -11,13 +11,6 @@ def _make_context():
 manager = Manager(app)
 
 manager.add_command("shell", Shell(make_context=_make_context))
-# Turn on debugger by default and reloader
-manager.add_command("runserver", Server(
-    use_debugger = True,
-    use_reloader = False,
-    host = '0.0.0.0',
-    threaded=False)
-)
 manager.add_command("disqus",DisqusCommand)
 
 if __name__ == "__main__":
