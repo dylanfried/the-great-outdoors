0470c05f4c0be5c7482a68f29cc2931b11fbbff4
Dylan Fried <dylanfried@Dylans-MacBook-Pro.local>
2016-04-16 12:59:27 -0400

    command to print out most popular posts

--- app/scripts/disqus.py
+++ app/scripts/disqus.py
@@ -1,16 +1,29 @@
 from flask import current_app as app
 from flask.ext.script import Command, Manager, Option
 from disqusapi import DisqusAPI
+import time
 
 class DisqusCommand(Command):
 
     option_list = (
+        Option("--popular_posts", "-pp", dest="popular_posts", default=False, action="store_true",
+                help='Print out list of popular posts'),
+        Option("--trending_threads", "-t", dest="trending_threads", default=False, action="store_true",
+                help='Print out list of trending threads'),
     )
 
-    def run(self):
-        # Disqus's documentation on the official Python bindings are wrong. I modified to seemingly work
-        # seems like you just chain on the resource and endpoint like so: `<API object>.resource.endpoint(args)`
+    def run(self,popular_posts=False,trending_threads=False):
         disqus = DisqusAPI(app.config['DISQUS']['API_SECRET'], app.config['DISQUS']['API_KEY'])
-        for result in disqus.trends.listThreads(method='GET'):
-            print result
+        if popular_posts:
+            app.logger.debug("Executing popular posts")
+            for result in disqus.posts.listPopular(method="GET"):
+                if result.get('message',None):
+                    print "AUTHOR:%s\nDATE:%s\nPOST:%s\n" % (result.get('author',{}).get('name','ANONYMOUS'),result.get('createdAt','N/A'),result.get('message','N/A'))
+                    time.sleep(3)
+        elif trending_threads:
+            app.logger.debug("Executing trending threads")
+            # Disqus's documentation on the official Python bindings are wrong. I modified to seemingly work
+            # seems like you just chain on the resource and endpoint like so: `<API object>.resource.endpoint(args)`
+            for result in disqus.trends.listThreads(method='GET'):
+                print result
 
